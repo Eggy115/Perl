33fe95a09d06abf368453bfcaf0f2d495f3951d6
Eggy115 <79756011+Eggy115@users.noreply.github.com>
2023-03-31 12:59:59 +0100

    Create send_email.pl

---
+++ send_email.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# first, create your message
+use Email::MIME;
+my $message = Email::MIME->create(
+  header_str => [
+    From    => 'eggy115@example.com',
+    To      => 'eggy116@example.com',
+    Subject => 'Happy birthday!',
+  ],
+  attributes => {
+    encoding => 'quoted-printable',
+    charset  => 'ISO-8859-1',
+  },
+  body_str => "Happy birthday to you!\n",
+);
+
+# send the message
+use Email::Sender::Simple qw(sendmail);
+sendmail($message);
